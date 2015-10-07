aa636fa36e5036774b7fa92bcc66b5fb232a9efd
Shalmali Padgaokar <shalmalidalvi@hotmail.com>
2015-10-06 21:18:15 -0400

    ADDED TRANSITION and BACKGROUND-COLOR.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $('button').on('click',function(event){
    btn.siblings().addClass("para");
   // $('list-class').removeClass('para');
   btn.parent().siblings().find(".para").removeClass("para").addClass('collapse-para');
-
+  // btn.parent().siblings().find("normal-button").css('background-color', '#00C7D9');
+btn.css('background-color','#00DBEE')
 
 });
